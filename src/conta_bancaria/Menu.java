5d61584266d2a6b3c6ec773d8817657477d04db7
Samara Almeida <samaraalmeida379@gmail.com>
2023-11-16 18:48:31 -0300

    Menu

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -1,10 +1,62 @@
 package conta_bancaria;
 
+import java.util.Scanner;
+import conta_bancaria.util.*;
+
 public class Menu {
+	static Scanner leia = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int opcao;
 
+		
+		while (true) {
+			System.out.println(Cores.TEXT_GREEN_BOLD + Cores.ANSI_BLACK_BACKGROUND);
+			System.out.println(" ******************************************** ");
+			System.out.println("                                              ");
+			System.out.println("                  RATANABÁ BANK               ");
+			System.out.println("                                              ");
+			System.out.println(" ******************************************** ");
+			System.out.println("                                              ");
+			System.out.println("        1 - Criar Conta                       ");
+			System.out.println("        2 - Listar todas as Contas            ");
+			System.out.println("        3 - Buscar Conta por Numero           ");
+			System.out.println("        4 - Atualizar Dados da Conta          ");
+			System.out.println("        5 - Apagar Conta                      ");
+			System.out.println("        6 - Sacar                             ");
+			System.out.println("        7 - Depositar                         ");
+			System.out.println("        8 - Transferir valores entre Contas   ");
+			System.out.println("        9 - Sair                              ");
+			System.out.println("                                              ");
+			System.out.println(" ******************************************** ");
+			System.out.println("          Entre com a opção desejada:         ");
+			System.out.println("                                              "+Cores.TEXT_RESET);
+			opcao = leia.nextInt();
+			if(opcao == 9) {
+				System.out.println("*** Ratanabá Bank ***");
+				sobre();
+				break;
+			}
+			switch(opcao) {
+			case 1 -> System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Criar conta \n\n");
+			case 2 -> System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Listar todas as contas \n\n");
+			case 3 -> System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Consultar dados da conta - por número\n\n");
+			case 4 -> System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Atualizar dados da conta\n\n");
+			case 5 -> System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Apagar a conta\n\n");
+			case 6 -> System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Saque\n\n");
+			case 7 -> System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Depósito\n\n");
+			case 8 -> System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND_BRIGHT+"Transferência entre contas\n\n");
+			default -> System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND_BRIGHT+"Opção inválida! \n");
+			}
+		}
+				
+
+	}
+	
+	public static void sobre() {
+		System.out.println("\n*******************************************");
+		System.out.println("Projeto desenvolvido por: ");
+		System.out.println("Samara - samaraalmeida379@gmail.com - github.com/als-samara");
 	}
 
 }
