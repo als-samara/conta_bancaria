d270adedbaa7bb4bd6692ea703ec79b5354f0fb0
Samara Almeida <samaraalmeida379@gmail.com>
2023-11-18 13:19:10 -0300

    Método keyPress

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -1,10 +1,12 @@
 package conta_bancaria;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
-import conta_bancaria.model.Conta;
-import conta_bancaria.util.Cores;
+
 import conta_bancaria.model.ContaCorrente;
 import conta_bancaria.model.ContaPoupanca;
+import conta_bancaria.util.Cores;
 
 public class Menu {
 	static Scanner leia = new Scanner(System.in);
@@ -48,22 +50,55 @@ public class Menu {
 			System.out.println(" ******************************************** ");
 			System.out.println("          Entre com a opção desejada:         ");
 			System.out.println("                                              "+Cores.TEXT_RESET);
-			opcao = leia.nextInt();
+			
+			try{
+				opcao = leia.nextInt();
+			}catch (InputMismatchException e) {
+				System.out.println("\nDigite valores inteiros!");
+				leia.nextLine();
+				opcao = 0;
+			}
 			if(opcao == 9) {
 				System.out.println("*** Ratanabá Bank ***");
 				sobre();
 				break;
 			}
 			switch(opcao) {
-			case 1 -> System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Criar conta \n\n");
-			case 2 -> System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Listar todas as contas \n\n");
-			case 3 -> System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Consultar dados da conta - por número\n\n");
-			case 4 -> System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Atualizar dados da conta\n\n");
-			case 5 -> System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Apagar a conta\n\n");
-			case 6 -> System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Saque\n\n");
-			case 7 -> System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Depósito\n\n");
-			case 8 -> System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND_BRIGHT+"Transferência entre contas\n\n");
-			default -> System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND_BRIGHT+"Opção inválida! \n");
+			case 1: System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Criar conta \n\n");
+			
+			keyPress();
+			break;
+			case 2: System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Listar todas as contas \n\n");
+			
+			keyPress();
+			break;
+			case 3: System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Consultar dados da conta - por número\n\n");
+			
+			keyPress();
+			break;
+			case 4: System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Atualizar dados da conta\n\n");
+			
+			keyPress();
+			break;
+			case 5: System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Apagar a conta\n\n");
+			
+			keyPress();
+			break;
+			case 6: System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Saque\n\n");
+			
+			keyPress();
+			break;
+			case 7: System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND+"Depósito\n\n");
+			
+			keyPress();
+			break;
+			case 8: System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND_BRIGHT+"Transferência entre contas\n\n");
+			
+			keyPress();
+			break;
+			default: System.out.println(Cores.TEXT_WHITE_BOLD+Cores.ANSI_GREEN_BACKGROUND_BRIGHT+"Opção inválida! \n");
+			keyPress();
+			break;
 			}
 		}
 				
@@ -74,6 +109,15 @@ public class Menu {
 		System.out.println("\n*******************************************");
 		System.out.println("Projeto desenvolvido por: ");
 		System.out.println("Samara - samaraalmeida379@gmail.com - github.com/als-samara");
+	}
+	
+	public static void keyPress() {
+		try {
+			System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para Continuar");
+			System.in.read();
+		}catch(IOException e) {
+			System.out.println("Você pressionou uma tecla diferente de Enter");
+		}
 	}
 
 }
